37acea8d03b2331d93e6a3e0ca280df99cfa0799
Guangchuang Yu <guangchuangyu@gmail.com>
2011-12-29 14:51:17 +0800

    kMeans algorithm

---
+++ R/MLclass-package.R
@@ -0,0 +1,34 @@
+#' statistical analysis and visulization of functional profiles for genes and
+#' gene clusters
+#' The package implements methods to analyze and visualize functional profiles
+#' of gene and gene clusters.
+#'
+#' This package is designed to implement the ML algorithms presented in ML Class (http://www.ml-class.org, Stanford).
+#'
+#' \tabular{ll}{ Package: \tab MLclass\cr Type: \tab Package\cr
+#' Version: \tab 0.1.0\cr Date: \tab 12-29-2011\cr Depends:\tab 
+#' ggplot2, methods\cr License: \tab Artistic-2.0\cr }
+#'
+#' @name MLclass-package
+#' @aliases MLclass-package MLclass
+#' @docType package
+#' @author Guangchuang Yu \email{guangchuangyu@@gmail.com}
+#'
+#' Maintainer: Guangchuang Yu \email{guangchuangyu@@gmail.com}
+#' @seealso \linkS4class{kMeansResult}, \link{kMeans}
+#' @keywords package
+NULL
+
+
+
+#' Datasets
+#' data samples.
+#'
+#'
+#' @name DataSet
+#' @aliases ex7data2
+#' @docType data
+#' @keywords datasets
+NULL
+
+
---
+++ R/kMeans.R
@@ -0,0 +1,187 @@
+##' Class "kMeansResult"
+##' This class represents the result of kmeans clustering.
+##'
+##'
+##' @name kMeansResult-class
+##' @aliases kMeansResult-class
+##'   getClusters, kMeansResult-method plot, kMeansResult-method
+##'
+##' @docType class
+##' @slot dataset original dataset
+##' @slot clusters clustering result
+##' @slot centroids final centroids
+##' @slot K K clusters
+##' @slot traceCentroids tracing the centroids when algorithm progress
+##' @seealso \code{\link{kMeans}}
+##' @keywords classes
+##' @author Guangchuang Yu \url{http://ygc.name}
+setClass("kMeansResult",
+         representation=representation(
+         dataset="matrix",
+         clusters="numeric",
+         centroids="matrix",
+         K="numeric",
+         traceCentroids="matrix"
+         )
+         )
+
+
+##' Randomly initial centroids for K clusters
+##'
+##' Randomly selected K features from dataset X
+##' @title kMeansInitCentroids
+##' @param X data matrix
+##' @param K numberic for setting K clusters
+##' @return randomly centroids
+##' @seealso \code{\link{kMeans}}
+##' @author Guangchuang Yu \url{http://ygc.name}
+kMeansInitCentroids <- function(X, K) {
+    rand.idx <- sample(1:nrow(X), K)
+    centroids <- X[rand.idx,]
+    return(centroids)
+}
+##' finding cloest centroids
+##'
+##'
+##' @title findClosestCentroids
+##' @param X dataset
+##' @param centroids centroids
+##' @return cluster index
+##' @author Guangchuang Yu \url{http://ygc.name}
+findClosestCentroids <- function(X, centroids) {
+    ## finding closest centroids
+
+    # set K
+    K <- nrow(centroids)
+
+    idx <- sapply(1:nrow(X), function(i) {
+        which.min(
+                  sapply(1:K, function(j) {
+                      sum(
+                          (X[i,]-centroids[j,])^2
+                          )
+                  })
+                  )
+    })
+    return(idx)
+}
+##' computing centroids
+##'
+##'
+##' @title computeCentroids
+##' @param X dataset
+##' @param idx cluster index
+##' @param K cluster number
+##' @return centroids
+##' @author Guangchuang Yu \url{http://ygc.name}
+computeCentroids <- function(X, idx, K) {
+    centroids <- sapply(1:K, function(i)
+                        colMeans(X[idx == i,]))
+    centroids <- t(centroids)
+    return(centroids)
+}
+
+##' kMeans algorithm
+##'
+##' kmeans algorithm
+##' @title kMeans
+##' @param X dataset
+##' @param centers initial centers or K
+##' @param max.iter maximum iterations
+##' @return A \code{kMeansResult} instance.
+##' @importFrom methods new
+##' @export
+##' @author Guangchuang Yu \url{http://ygc.name}
+##' @keywords manip
+kMeans <- function(X, centers, max.iter = 10){
+    if(length(centers) == 1L) {
+        K <- centers
+        initCentroids <- kMeansInitCentroids(X, K)
+    } else {
+        initCentroids <- centers
+    }
+
+    K <- nrow(initCentroids)
+    centroids <- initCentroids
+    preCentroids <- centroids
+
+    for (i in 1:max.iter) {
+        idx <- findClosestCentroids(X, centroids)
+        centroids <- computeCentroids(X, idx, K)
+        preCentroids <- rbind(preCentroids, centroids)
+    }
+
+    new("kMeansResult",
+        dataset=X,
+        clusters=idx,
+        centroids=centroids,
+        K=K,
+        traceCentroids=preCentroids
+        )
+}
+
+
+##' @exportMethod plot
+setGeneric("plot", function(object, ...) standardGeneric("plot"))
+
+##' @exportMethod getClusters
+setGeneric("getClusters", function(object) standardGeneric("getClusters"))
+
+##' plot method for \code{kMeansResult} instance
+##'
+##' @name plot
+##' @docType methods
+##' @rdname plot-methods
+##'
+##' @title plot method
+##' @param object A \code{kMeans} instance
+##' @param trace tracing centroids when algorithm progress
+##' @return graph
+##' @author Guangchuang Yu \url{http://ygc.name}
+setMethod("plot", signature(object="kMeansResult"),
+          function (object, trace=F) {
+              require(ggplot2)
+              X = object@dataset
+              colnames(X) <- c("V1", "V2")
+              if(ncol(X) != 2) {
+                  stop("plot function only support two features in dataset.")
+              }
+              idx <- object@clusters
+              xx <- data.frame(X, cluster=as.factor(idx))
+              p <- ggplot(xx, aes(V1, V2))+geom_point(aes(color=cluster))
+              if (trace) {
+                  preCentroids <- object@traceCentroids
+                  preCentroids <- data.frame(preCentroids,
+                                             idx=rep(1:3, nrow(preCentroids)/3))
+                  p <- p+geom_point(data=preCentroids,
+                                    aes(x=V1, y=V2)) +
+                                        geom_path(data=preCentroids,
+                                                  aes(x=V1, y=V2, group=idx))
+              }
+              print(p)
+          }
+          )
+
+##' getClusters method for \code{kMeansResult} instance
+##'
+##' @name getClusters
+##' @docType methods
+##' @rdname getClusters-methods
+##'
+##' @title getCluster method
+##' @param object A \code{kMeansResult} instance.
+##' @return cluster index
+##' @author Guangchuang Yu \url{http://ygc.name}
+setMethod("getClusters", signature(object="kMeansResult"),
+          function(object) {
+              clusters <- object@clusters
+              return(clusters)
+          }
+          )
+
+
+## dataset was converted from ML-class exercise 7  ex7data2.mat.
+##X <- load("ex7data2")
+##K <- 3
+##xx <- kMeans(X,K)
+
